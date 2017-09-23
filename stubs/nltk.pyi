b0e55299cd9f7bb12486c7b6972f07f61b0d8745
Mark Koh <markkohdev@gmail.com>
2017-09-23 13:04:06 -0400

    Add more reporting to tox.

--- stubs/nltk.pyi
+++ stubs/nltk.pyi
@@ -4,6 +4,5 @@ from typing import Sequence, Optional, Tuple
 def word_tokenize(text: str, language: str = 'english', preserve_line: bool = False) -> Sequence[
     str]: ...
 
-
 def pos_tag(tokens: Sequence[str], tagset: Optional[str] = None, lang: str = 'eng') -> Sequence[
     Tuple[str, str]]: ...
